f39aaa37c67c9f726b44132619044575f0afe34f
vishalkhade7 <58898040+vishalkhade7@users.noreply.github.com>
2019-12-15 10:02:26 +0530

    Add files via upload

---
+++ OnlineShopppingCart/onlineshoppingdatabase.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shoppingdb
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `USER_NAME` varchar(20) NOT NULL,
+  `ACTIVE` bit(1) NOT NULL,
+  `ENCRYTED_PASSWORD` varchar(128) NOT NULL,
+  `USER_ROLE` varchar(20) NOT NULL,
+  PRIMARY KEY (`USER_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES ('employee1',_binary '','$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu','ROLE_EMPLOYEE'),('manager1',_binary '','$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu','ROLE_MANAGER');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_details`
+--
+
+DROP TABLE IF EXISTS `order_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_details` (
+  `ID` varchar(50) NOT NULL,
+  `AMOUNT` double NOT NULL,
+  `PRICE` double NOT NULL,
+  `QUANITY` int(11) NOT NULL,
+  `ORDER_ID` varchar(50) NOT NULL,
+  `PRODUCT_ID` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ORDER_DETAIL_ORD_FK` (`ORDER_ID`),
+  KEY `ORDER_DETAIL_PROD_FK` (`PRODUCT_ID`),
+  CONSTRAINT `ORDER_DETAIL_ORD_FK` FOREIGN KEY (`ORDER_ID`) REFERENCES `orders` (`ID`),
+  CONSTRAINT `ORDER_DETAIL_PROD_FK` FOREIGN KEY (`PRODUCT_ID`) REFERENCES `products` (`CODE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_details`
+--
+
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `ID` varchar(50) NOT NULL,
+  `AMOUNT` double NOT NULL,
+  `CUSTOMER_ADDRESS` varchar(255) NOT NULL,
+  `CUSTOMER_EMAIL` varchar(128) NOT NULL,
+  `CUSTOMER_NAME` varchar(255) NOT NULL,
+  `CUSTOMER_PHONE` varchar(128) NOT NULL,
+  `ORDER_DATE` datetime NOT NULL,
+  `ORDER_NUM` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ORDER_UK` (`ORDER_NUM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `CODE` varchar(20) NOT NULL,
+  `IMAGE` longblob,
+  `NAME` varchar(255) NOT NULL,
+  `PRICE` double NOT NULL,
+  `CREATE_DATE` datetime NOT NULL,
+  PRIMARY KEY (`CODE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES ('S001',NULL,'CORE JAVA',100,'2019-11-10 23:59:59'),('S002',NULL,'Spring for Beginners',50,'2019-11-10 23:59:59'),('S003',NULL,'Swift for Beginners',120,'2019-11-10 23:59:59'),('S004',NULL,'Oracle XML Parser',120,'2019-11-10 23:59:59'),('S005',NULL,'CSharp Tutorial for Beginers',110,'2019-11-10 23:59:59');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-15  9:55:22
---
+++ OnlineShopppingCart/src/main/java/com/Springboot/shopping/OnlineShopppingCart/dao/AccountDAO.java
@@ -0,0 +1,24 @@
+package com.Springboot.shopping.OnlineShopppingCart.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.Springboot.shopping.OnlineShopppingCart.entiry.Account;
+ 
+@Transactional
+@Repository
+public class AccountDAO {
+ 
+    @Autowired
+    private SessionFactory sessionFactory;
+ 
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+ 
+}
